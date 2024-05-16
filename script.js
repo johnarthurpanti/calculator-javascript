69262aec15f7376417d3709b4304021016f49132
jorthan <78148547+johnarthurpanti@users.noreply.github.com>
2024-05-16 10:24:35 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const display = document.getElementById('display');
+let resultDisplayed = false;
+
+function appendToDisplay(input) {
+    if (resultDisplayed) {
+        clearDisplay();
+        resultDisplayed = false;
+    }
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function erase() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+        resultDisplayed = true;
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
